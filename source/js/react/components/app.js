59e92b28d722c0ebeadec97ce8a741436f8ae635
Andrew <andikk@mail.ru>
2020-10-31 18:10:32 +0300

    Добавил модальное окно

--- source/js/react/components/app.js
+++ source/js/react/components/app.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { Map, TileLayer, Marker, Popup} from 'react-leaflet'
 import MarkerClusterGroup from "react-leaflet-markercluster";
+import Modal from 'react-modal';
 
 const App = () => {
-
+  const [modalIsOpen,setIsOpen] = useState(false);
   const objects = [
     {
       id: "1",
@@ -19,20 +20,41 @@ const App = () => {
     },
   ];
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const openModal =  () => {
+    setIsOpen(true);
+  };
+
   return (
-    <Map center={[45.080572, 41.953576]} zoom={10}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      />
-      <MarkerClusterGroup>
-        {objects.map((object) => (
-          <Marker position={[object.lat, object.lon]}>
-            <Popup><h3>{object.name}</h3></Popup>
-          </Marker>
-        ))}
-      </MarkerClusterGroup>
-    </Map>
+    <>
+      <Map center={[45.080572, 41.953576]} zoom={10}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        <MarkerClusterGroup>
+          {objects.map((object) => (
+            <Marker position={[object.lat, object.lon]}>
+              <Popup>
+                <h3>{object.name}</h3>
+                <button className="button" onClick={openModal}>Подробнее</button>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </Map>
+      <Modal
+        isOpen={modalIsOpen}
+        contentLabel="Example Modal"
+        onRequestClose={closeModal}
+      >
+        <h1>Test</h1>
+      </Modal>
+    </>
+
   )
 };
 
